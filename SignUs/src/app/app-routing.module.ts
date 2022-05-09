6a3fdb9e570b7224d51535e10e0acf4f2b9a3681
GAGOER <fabian.afiankoe@gmail.com>
2022-05-09 10:44:54 -0300

    front pour balda

---
+++ SignUs/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'inscription',
+    loadChildren: () => import('./inscription/inscription.module').then( m => m.InscriptionPageModule)
+  },
+  {
+    path: 'changement-de-mot-passe',
+    loadChildren: () => import('./changement-de-mot-passe/changement-de-mot-passe.module').then( m => m.ChangementDeMotPassePageModule)
+  },
+  {
+    path: 'changement-information',
+    loadChildren: () => import('./changement-information/changement-information.module').then( m => m.ChangementInformationPageModule)
+  },
+  {
+    path: 'recapitulatif-du-signallement',
+    loadChildren: () => import('./recapitulatif-du-signallement/recapitulatif-du-signallement.module').then( m => m.RecapitulatifDuSignallementPageModule)
+  },
+  {
+    path: 'signallement',
+    loadChildren: () => import('./signallement/signallement.module').then( m => m.SignallementPageModule)
+  },
+  {
+    path: 'mot-de-passe-oublier',
+    loadChildren: () => import('./mot-de-passe-oublier/mot-de-passe-oublier.module').then( m => m.MotDePasseOublierPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
